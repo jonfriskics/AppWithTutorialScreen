c0af85ecc8f4d3edd87e05a00a18a7ce4c6fb438
Jon Friskics <jonfriskics@gmail.com>
2015-01-02 19:37:38 -0500

    Initial Commit

---
+++ AppWithTutorialScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppWithTutorialScreen
+//
+//  Created by Jon Friskics on 1/2/15.
+//  Copyright (c) 2015 Jon Friskics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
